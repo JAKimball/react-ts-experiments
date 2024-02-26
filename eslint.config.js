1ae5b7ef2a38bf09a8c8317079c4ef56c190931a
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2024-02-26 02:19:38 -0800

    chore(WIP): rename eslint.config.not_yet.js to eslint.config.js and add typescript-eslint and globals packages

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,12 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 
 export default tseslint.config(
-  eslint.configs.recommended,
   {
     // Set global ignore patterns (by specifying without other keys, this pattern is added after the default ignore patterns, so it's not necessary to include the default ignore patterns here, but it's not harmful to do so either.)
     ignores: [
@@ -20,12 +20,16 @@ export default tseslint.config(
       'pnpm-lock.yaml',
     ],
   },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  // ...react.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-      project: ['./tsconfig.json', './tsconfig.node.json'],
-      tsconfigRootDir: __dirname,
+      // project: ['./tsconfig.json', './tsconfig.node.json'],
+      // tsconfigRootDir: __dirname,
       globals: {
         ...globals.browser,
         ...globals.es2021,
